e683d2fd9791f765a44258657079a2ef62bc20b1
mollys1 <mollys1@iastate.edu>
2015-09-28 13:06:12 -0500

    add to gitignore & creating test case .java files

--- Homework2/src/test/Test.java
+++ Homework2/src/test/Test.java
@@ -1,5 +1,53 @@
 package test;
 
-public class Test {
-
+public class Test 
+{
+	//field tests
+	public int TestField;
+	public int TestDeletedField;
+	
+	//test field initializers
+	public int Test = 0;
+	public int TestChangeValue = 1;
+	public int TestDelete = 5;
+	public int TestModifierChange = 14;
+	
+	
+	public void TestMethod1()
+	{
+		return;
+	}
+	
+	public void TestMethod2()
+	{
+		return;
+	}
+	
+	public void TestMethod3()
+	{
+		return;
+	}
+	
+	public void TestMethod_ToChange()
+	{
+		TestField =  1;
+		TestField++;
+		return;
+	}
+	
+	public void TestMethod_ToDelete()
+	{
+		return;
+	}
+	
+	public void TestMethod4()
+	{
+		return;
+	}
+	
+	public void TestMethod_ToMove()
+	{
+		return;
+	}
+	
 }
---
+++ Homework2/src/test/TestChanged.java
@@ -0,0 +1,49 @@
+package test;
+
+public class TestChanged 
+{
+	//field tests
+	public int TestField;
+	public int TestAddedField;
+
+	//test field initializers
+	public int Test = 0;
+	public int TestChangeValue = 3;
+	private int TestModifierChange = 14;
+	
+	public void TestMethod1()
+	{
+		return;
+	}
+	
+	public void TestNewMethod()
+	{
+		
+	}
+	
+	public void TestMethod_ToMove()
+	{
+		return;
+	}
+	
+	public void TestMethod2()
+	{
+		return;
+	}
+	
+	public void TestMethod3()
+	{
+		return;
+	}
+	
+	public void TestMethod_ToChange()
+	{
+		TestField =  1;
+		return;
+	}
+	
+	public void TestMethod4()
+	{
+		return;
+	}
+}
