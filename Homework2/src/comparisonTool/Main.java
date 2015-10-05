65dc7c5da9f4839319ca2bc7635b81791d86176d
mollys1 <mollys1@iastate.edu>
2015-10-04 19:50:47 -0500

    Changed main to take arguments from command line

--- Homework2/src/comparisonTool/Main.java
+++ Homework2/src/comparisonTool/Main.java
@@ -6,10 +6,28 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		ComparisonTool compareTool = new ComparisonTool();
-		String testFile = "C:\\Users\\Molly\\Documents\\GitHub\\329Projects\\Homework2\\src\\test\\Test.java";
-		String testChangedFile = "C:\\Users\\Molly\\Documents\\GitHub\\329Projects\\Homework2\\src\\test\\TestChanged.java";
-		System.out.println(compareTool.Compare(testFile, testChangedFile));
+		String testFile, testChangedFile;
+		if (args.length == 3) //if files provided at the command line
+		{
+			testFile = args[1];
+			testChangedFile = args[2];
+			ComparisonTool compareTool = new ComparisonTool();
+			try
+			{
+				System.out.println(compareTool.Compare(testFile, testChangedFile));
+			}
+			catch (Exception ex)
+			{
+				System.out.println("An exception occurred:");
+				System.out.println(ex.getMessage());
+			}
+		}
+		else
+		{
+			System.out.println("Please specify two files to compare");
+		}
+//		testFile = "C:\\Users\\Molly\\Documents\\GitHub\\329Projects\\Homework2\\src\\test\\Test.java";
+//		testChangedFile = "C:\\Users\\Molly\\Documents\\GitHub\\329Projects\\Homework2\\src\\test\\TestChanged.java";
 	}
 
 }
