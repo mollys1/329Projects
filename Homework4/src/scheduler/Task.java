944c0e971a12683d1dcc99d6ea453a0320827ad7
mollys1 <mollys1@iastate.edu>
2015-10-25 21:48:47 -0500

    Finished code to find circular dependencies

--- Homework4/src/scheduler/Task.java
+++ Homework4/src/scheduler/Task.java
@@ -14,4 +14,20 @@ public class Task
 	String Name;
 	int Duration;
 	ArrayList<String> Dependencies;
+	
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Task)
+		{
+			Task task = (Task) obj;
+			if (task.Name == this.Name && task.Duration == this.Duration && task.Dependencies.equals(this.Dependencies))
+			{
+				return true;
+			}
+			else return false;
+		}
+		else return false;
+	}
 }
